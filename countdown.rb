d6810c65c834c13a5195013a8a37b58f31e0c480
Romano Livi <romano_livi@outlook.com>
2021-01-11 17:41:28 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,16 +4,15 @@ def countdown(number)
   while number > 0 
     puts "#{number} SECOND(S)!"
     number -= 1 
+    countdown_with_sleep(number)
   end
   
   return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(number)
-  countdown(number)
-  sleep 5
-  
+  sleep 1
 end
 
 
-puts countdown_with_sleep(number)
+puts countdown(number)
